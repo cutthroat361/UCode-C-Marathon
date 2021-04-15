14b6658ce84ffbe8796ae2adf6f7545cb4a3ed40
EKorol <60097656+cutthroat361@users.noreply.github.com>
2021-04-15 13:43:30 +0300

    Delete Sprint02/ekorol directory

--- Sprint01/ekorol/t01/mx_write_knock_knock.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-
-void mx_write_knock_knock()
-{
-    char text[]= "Wake up, NEO \\ (^_^) /\nThe Matrix has you";
-    write(1, text, strlen(text));
-}
